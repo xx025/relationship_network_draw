79cf12667bbe988ec919ef471f631cf1383b2b70
xx025 <71559822+xx025@users.noreply.github.com>
2022-02-08 18:13:08 +0800

    Update

--- static/js/register.js
+++ static/js/register.js
@@ -1,29 +1,14 @@
 $(document).ready(function () {
+
+
     $("#register-btn").click(function () {
-        window.location.href = "/register"
-    });
+        $("#register").css("display", 'none');
+        $("#a_acc").css("display", 'block');
+    })
 
-
-    function login() {
-        let username = $("#id").val();
-        let password = $("#password").val()
-        console.log(username)
-        console.log(password)
-
-    }
-
-    $("#login-btn").click(function () {
-        login();
-    });
-    $(document).keypress(function (event) {
-        //    回车键，绑定登录方法
-        if (event.keyCode == 13) {
-            login();
-        }
-    });
-
-    $("#login").click(function () {
+    $(".login_link").click(function () {
         window.location.href = "/login"
+
     });
 
 
