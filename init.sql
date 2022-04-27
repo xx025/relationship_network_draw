8533bf0f3b5d5037002d87a917f10099e519b454
xx025 <71559822+xx025@users.noreply.github.com>
2022-04-27 11:48:05 +0800

    创建 用户到文件关联数据库表格

--- init.sql
+++ init.sql
@@ -4,11 +4,19 @@ create table USER
     email    text PRIMARY KEY,
     password text
 );
+-- 创建用户表
 
-INSERT INTO USER (email, password)
-VALUES ('chongfu@test.com', '11223344');
--- 向数据库插入邮箱chongfu@test.com 用于注册时账户已注册校验
+drop table if exists USER_FILE;
+Create table USER_FILE
+(
+    user  text,
+    fpath text primary key,
+    fname text,
+    constraint user_key foreign key (user) references USER (email)
+);
+-- 创建用户文件关联表
 
 INSERT INTO USER (email, password)
 VALUES ('ds226688@aliyun.com', '7e5c1cfc2079877c5d961450084831aedd6c7c6c3e7ffca9605d5a04b097359d');
--- -- 向数据库插入邮箱chongfu@test.com 用于注册时账户已注册校验
+-- -- 插入一个邮箱，【密码正确】
+
